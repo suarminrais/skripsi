a1ca121324bedef1abc044fcf0348a9601473311
Suarmin Rais <suarmin.rais@gmail.com>
2022-03-07 18:41:51 +0800

    user fixed

--- backend/app/Http/Controllers/Api/v1/UserController.php
+++ backend/app/Http/Controllers/Api/v1/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api\v1;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
