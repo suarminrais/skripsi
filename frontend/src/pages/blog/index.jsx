b941622e02f5f27873133619327ea9eb3d6f226f
Suarmin Rais <suarmin.rais@gmail.com>
2022-03-08 14:13:51 +0800

    program admin

--- frontend/src/pages/blog/index.jsx
+++ frontend/src/pages/blog/index.jsx
@@ -1,13 +1,80 @@
 import BlogCard from "@/components/blog/blog.component";
+import DataTable from "@/components/datatable/datatable.component";
 import Footer from "@/components/footer/footer.component";
 import Navbar from "@/components/navbar/navbar.component";
 import Section from "@/components/section/section.components";
 import { useAuth } from "@/hooks/auth";
 import Head from "next/head";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import _ from 'lodash';
+import Modal from "@/components/modal/modal.component";
+import { Form } from "@/components/form/form.component";
+import { InputLabel } from "@/components/form/form.component";
+import { convertToRaw, EditorState } from 'draft-js';
+import draftToHtml from 'draftjs-to-html';
+import dynamic from "next/dynamic";
+import { useModal } from "@/hooks/modal";
+import DataTableWrapper from "@/components/datatable/datatable-wrapper.component";
 
+const EditorForm = dynamic(() => import('../../components/form/form-editor.component'), { ssr: false });
 const Blog = () => {
-  const { user, logout } = useAuth({ middleware: 'guest' });
+  const { user, logout, blog, createBlog } = useAuth({ middleware: 'guest' });
+  const [show, handleClick] = useModal();
+  const [blogs, setBlogs] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [name, setName] = useState('')
+  const [image, setImage] = useState('')
+  const [description, setWysiwyg] = useState(EditorState.createEmpty());
+  const [errors, setErrors] = useState([]);
+
+  useEffect(() => {
+    if (errors.length > 0) alert(errors)
+  }, [errors]);
+
+  useEffect(() => {
+    setBlogs(_.slice(blog?.data, 0, 3))
+  }, [blog]);
+
+  if (user?.type === 'admin') {
+    const handleSubmit = async () => {
+      const formData = new FormData();
+      formData.append('name', name)
+      formData.append('image', image)
+      formData.append('description', draftToHtml(convertToRaw(description?.getCurrentContent())))
+      setLoading(true)
+      await createBlog({ formData, setErrors, handleClick })
+      setLoading(false)
+    }
+
+    const columns = [
+      { title: "Id", field: "id", width: 70 },
+      { title: "Name", field: "name" },
+      { title: "Description", field: "description" },
+      { title: "Action", width: 180 },
+    ];
+
+    return (
+      <>
+        <Head>
+          <title>Modal Tani - Blog</title>
+        </Head>
+        <Navbar user={user} logout={logout} />
+        <DataTableWrapper title="Data Blog" handleClick={handleClick}>
+          {
+            (blog && blog?.data.length > 0) && <DataTable data={blog?.data} columns={columns} />
+          }
+        </DataTableWrapper>
+        <Modal show={show} loading={loading} handleClick={handleClick} title="Buat Blog" handleSubmit={handleSubmit}>
+          <Form>
+            <InputLabel value={name} onChange={e => setName(e.target.value)} label="Nama Blog" type="text" />
+            <InputLabel onChange={e => setImage(e.target.files[0])} label="Foto Blog" type="file" accept="image/*" />
+            <EditorForm state={description} setState={setWysiwyg} />
+          </Form>
+        </Modal>
+        <Footer />
+      </>
+    );
+  }
 
   return (
     <>
@@ -15,10 +82,16 @@ const Blog = () => {
         <title>Modal Tani - Blog</title>
       </Head>
       <Navbar user={user} logout={logout} />
-      <Section title="Blog Modal Tani" subtitle="Bantu Petani, Pilih Program Pertanian. Modali dan Bagi Hasilnya." total="30">
-        <BlogCard id={1} image="https://bucket.tanifund.com/uploads/projects/58d55ebd-9e45-4882-a85a-2b060209fc4d/280222030745-image.jpg" title="Budidaya" body="asdasdasdasdasd" />
-        <BlogCard id={1} image="https://bucket.tanifund.com/uploads/projects/58d55ebd-9e45-4882-a85a-2b060209fc4d/280222030745-image.jpg" title="Budidaya" body="asdasdasdasdasd" />
-        <BlogCard id={1} image="https://bucket.tanifund.com/uploads/projects/58d55ebd-9e45-4882-a85a-2b060209fc4d/280222030745-image.jpg" title="Budidaya" body="asdasdasdasdasd" />
+      <Section title="Blog Modal Tani" subtitle="Bantu Petani, Pilih Program Pertanian. Modali dan Bagi Hasilnya." total="3">
+        {
+          blogs?.length > 0 && blogs.map(data => <BlogCard
+            key={data.id}
+            id={data.id}
+            image={`${process.env.NEXT_PUBLIC_BACKEND_URL}/storage/images/${data.image.name}`}
+            title={data.name}
+            body={data.description}
+          />)
+        }
       </Section>
       <Footer />
     </>
